107e70c6c2ef206f1bae4a8962f227928fa49c38
armageddion <armageddion@gmail.com>
2016-01-03 13:47:33 -0500

    initial commit

---
+++ app/src/test/java/io/alfr3d/alfr3d_mkiv/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package io.alfr3d.alfr3d_mkiv;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
